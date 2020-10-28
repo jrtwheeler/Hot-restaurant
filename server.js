5965775a5daedfef3160b9c5f35f5675010fc344
ericw142 <66480663+ericw142@users.noreply.github.com>
2020-10-28 19:27:28 -0400

    initial route handling

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const table = require('./data/tabledata');
+const waitlist = require('./data/waitlist');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Route Handling
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/table", function(req, res) {
+  res.sendFile(path.join(__dirname, "table.html"));
+});
+// APIs
+app.get("/api/reservations", function(req, res) {
+  return res.json(table);
+});
+
+app.get("/api/waitlist", function(req, res) {
+  return res.json(waitlist);
+})
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
