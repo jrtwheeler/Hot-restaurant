16655e3962ab2b5bad39c536225f10bc931c741e
ericw142 <66480663+ericw142@users.noreply.github.com>
2020-10-28 19:10:19 -0400

    basic server functionality

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
